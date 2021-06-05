e90de7e0329b84abb194fb06a0eaeb2ba995c003
changwn <changwn.nku@gmail.com>
2021-06-05 18:33:27 -0400

    add expr K

--- expr_simu.R
+++ expr_simu.R
@@ -1,8 +1,10 @@
 
-library(RobMixReg)
+# library(RobMixReg)
 library(robustbase)
 source('./Prof_pipeline3_roubst_lib.R')
 source('./expr_simu_lib')
+
+library(tictoc)
 
 simu_list = list() # outside wrapper
 
@@ -31,41 +33,48 @@ for(i in 1:100){
   dat_simu1 = simulateData_outlier_n(n=c(40,40,20), beta=c(1.5,1.0), coordinate = c(1,1, -1,-1))
   group_1[[i]] = dat_simu1
   
-  dat_simu1 = simulateData_outlier_n(n=c(200,200,100), beta=c(1.5,1.0), coordinate = c(1,1, -1,-1))
+  dat_simu1 = simulateData_outlier_n(n=c(80,80,40), beta=c(1.5,1.0), coordinate = c(1,1, -1,-1))
   group_2[[i]] = dat_simu1
   
-  dat_simu1 = simulateData_outlier_n(n=c(400,400,200), beta=c(1.5,1.0), coordinate = c(1,1, -1,-1))
+  dat_simu1 = simulateData_outlier_n(n=c(160,160,80), beta=c(1.5,1.0), coordinate = c(1,1, -1,-1))
   group_3[[i]] = dat_simu1
 }
 simu_n = list(group_1, group_2, group_3)
 names(simu_n) = c('n=100', 'n=500', 'n=1000')
 
-dat_simu1 = simu_n[[2]][[100]]
-data = data.frame(dat_simu1$mat)
-xy = dat_simu1$xy
-cl = dat_simu1$cl
-plot(data, col=cl, pch=16)
-plot(xy, col=cl, pch=16)
-RobSpa.res = RobSpaReg(formula = as.formula("y~x"), data, nit=20, nc=2, rlr_method="ltsReg", Cdn=xy, lamb=5)
-RobSpa.res@inds_in
-RobSpa.res@indout
-RobSpa.res@ctleclusters[1:200]
-RobSpa.res@ctleclusters[201:400]
-RobSpa.res@ctleclusters[401:500]
 
 #----K-----
 group_1 = list()
 group_2 = list()
 group_3 = list()
 for(i in 1:100){
-  dat_simu1 = simulateData_outlier_n(n=c(40,40,20), beta=c(1.5,1.0), coordinate = c(1,1, -1,-1))
+  dat_simu1 = simulateData_outlier_k(k=2)
   group_1[[i]] = dat_simu1
   
-  dat_simu1 = simulateData_outlier_n(n=c(200,200,100), beta=c(1.5,1.0), coordinate = c(1,1, -1,-1))
+  dat_simu1 = simulateData_outlier_k(k=3)
   group_2[[i]] = dat_simu1
   
-  dat_simu1 = simulateData_outlier_n(n=c(400,400,200), beta=c(1.5,1.0), coordinate = c(1,1, -1,-1))
+  dat_simu1 = simulateData_outlier_k(k=4)
   group_3[[i]] = dat_simu1
 }
 simu_k = list(group_1, group_2, group_3)
-names(simu_n) = c('k=100', 'k=500', 'k=1000')
+names(simu_k) = c('k=2', 'k=3', 'k=4')
+
+# dat_simu1 = simu_k[[3]][[100]]
+# data = data.frame(dat_simu1$mat)
+# xy = dat_simu1$xy
+# cl = dat_simu1$cl
+# plot(data, col=cl, pch=16)
+# plot(xy, col=cl, pch=16)
+# tic()
+# RobSpa.res = RobSpaReg(formula = as.formula("y~x"), data, nit=20, nc=4, rlr_method="ltsReg", Cdn=xy, lamb=5)
+# toc()
+# RobSpa.res@inds_in
+# RobSpa.res@indout
+# RobSpa.res@ctleclusters[1:40]
+# RobSpa.res@ctleclusters[41:80]
+# RobSpa.res@ctleclusters[81:120]
+# RobSpa.res@ctleclusters[121:160]
+
+
+
--- expr_simu_lib.R
+++ expr_simu_lib.R
@@ -1,4 +1,4 @@
-simulateData_outlier_sig <- function(n=c(40,40,20), beta=c(1.5,-1.5), sigma=0.1, 
+simulateData_outlier_sig <- function(n=c(40,40,20), beta=c(1.5,-1.5), sigma=0.05, 
                                      coordinate=c(1,1,-1,-1) 
 )
 {
@@ -25,6 +25,7 @@ simulateData_outlier_sig <- function(n=c(40,40,20), beta=c(1.5,-1.5), sigma=0.1,
   
   return(list(mat=mat, xy=xy, cl=cl))
 }
+
 
 simulateData_outlier_n <- function(n=c(40,40,20), beta=c(1.5,-1.5), sigma=0.05, 
                                    coordinate=c(1,1,-1,-1) 
@@ -54,6 +55,132 @@ simulateData_outlier_n <- function(n=c(40,40,20), beta=c(1.5,-1.5), sigma=0.05,
   ccc.n1 = rbind(ccc[loca.in,], ccc2[loca.out,])
   ccc.n2 = rbind(ccc2[loca.in,], ccc[loca.out,])
   xy = rbind(ccc.n1, ccc.n2,ccc.o1, ccc.o2)
+  
+  return(list(mat=mat, xy=xy, cl=cl))
+}
+
+
+simu_outlier_k <- function(vv, beta=NULL, distOut = 0.5){
+  out = rep(0, length(vv))
+  i = 1
+  n_comp = length(beta)
+  
+  while(i<= length(vv)){
+    acept=TRUE
+    tmp = sample(seq(-8, 8,0.01),1)
+    for(j in 1:n_comp){
+      if(abs(tmp - beta[j]*vv[i])<=distOut) acept=FALSE
+    }
+    if(acept){ #reject if tmp close to mixture lines.
+      out[i] = tmp
+      i = i+1
+    }
+  }
+  return(out)
+}
+
+simulateData_outlier_k <- function(k = 2)
+{
+  if(! k%in% c(2,3,4) ) stop('k can only be 2,3,4!')
+  if(k == 2){
+    n=c(40,40,20);
+    beta=c(1.5,1.0)
+    sigma=0.05 
+    coordinate=c(1,1,-1,-1)
+    
+    cl = c(rep(1,35), rep(4, 5), 
+           rep(2,35), rep(5, 5), 
+           rep(3, 20))
+    
+    mat = matrix(runif(sum(n)*2,-2,2), sum(n), 2)
+    colnames(mat) = c('y','x')
+    loca1 = 1:40;  loca2 = 41:80;  loca3 = 81:100
+    mat[loca1,1] = beta[1] * mat[loca1,2] + rnorm(40,0,sigma)
+    mat[loca2,1] = beta[2] * mat[loca2,2] + rnorm(40,0,sigma)
+    mat[loca3,1] = simu_outlier(mat[loca3, 2], beta, distOut=2)
+    mat = mat[,2:1] #change order
+    
+    # spatial coordinate
+    ccc = mvrnorm(n[1], mu = c(coordinate[1],coordinate[2]), Sigma=diag(0.1, 2,2))
+    ccc2 = mvrnorm(n[2], mu = c(coordinate[3],coordinate[4]), Sigma=diag(0.1, 2,2))
+    ccc.o1 = mvrnorm(n[3]/2, mu = c(coordinate[1],coordinate[2]), Sigma=diag(0.1, 2,2))
+    ccc.o2 = mvrnorm(n[3]/2, mu = c(coordinate[3],coordinate[4]), Sigma=diag(0.1, 2,2))
+    ccc.n1 = rbind(ccc[1:35,], ccc2[36:40,])
+    ccc.n2 = rbind(ccc2[1:35,], ccc[36:40,])
+    xy = rbind(ccc.n1, ccc.n2,ccc.o1, ccc.o2)
+  }
+  
+  if(k == 3){
+    n=c(40,40,40,20);
+    beta=c(3,1.5,-3)
+    sigma=0.05 
+    coordinate=c(1,1, -1,-1, -1,1)
+    
+    cl = c(rep(1,35), rep(5, 5), 
+           rep(2,35), rep(6, 5), 
+           rep(3,35), rep(7, 5), 
+           rep(4, 20))
+    
+    mat = matrix(runif(sum(n)*2,-2,2), sum(n), 2)
+    colnames(mat) = c('y','x')
+    loca1 = 1:40;  loca2 = 41:80;  loca3 = 81:120; loca4 = 121:140
+    mat[loca1,1] = beta[1] * mat[loca1,2] + rnorm(40,0,sigma)
+    mat[loca2,1] = beta[2] * mat[loca2,2] + rnorm(40,0,sigma)
+    mat[loca3,1] = beta[3] * mat[loca3,2] + rnorm(40,0,sigma)
+    mat[loca4,1] = simu_outlier_k(mat[loca4, 2], beta, distOut=2)
+    mat = mat[,2:1] #change order
+    
+    # spatial coordinate
+    ccc = mvrnorm(n[1], mu = c(coordinate[1],coordinate[2]), Sigma=diag(0.1, 2,2))
+    ccc2 = mvrnorm(n[2], mu = c(coordinate[3],coordinate[4]), Sigma=diag(0.1, 2,2))
+    ccc3 = mvrnorm(n[3], mu = c(coordinate[5],coordinate[6]), Sigma=diag(0.1, 2,2))
+    ccc.o1 = mvrnorm(7, mu = c(coordinate[1],coordinate[2]), Sigma=diag(0.1, 2,2))
+    ccc.o2 = mvrnorm(7, mu = c(coordinate[3],coordinate[4]), Sigma=diag(0.1, 2,2))
+    ccc.o3 = mvrnorm(6, mu = c(coordinate[5],coordinate[6]), Sigma=diag(0.1, 2,2))
+    ccc.n1 = rbind(ccc[1:35,], ccc2[36:40,])
+    ccc.n2 = rbind(ccc2[1:35,], ccc3[36:40,])
+    ccc.n3 = rbind(ccc3[1:35,], ccc[36:40,])
+    xy = rbind(ccc.n1,ccc.n2,ccc.n3,  ccc.o1,ccc.o2,ccc.o3)
+  }
+  
+  if(k == 4){
+    n=c(40,40,40,40,20);
+    beta=c(3,1.5,-1.5,-3)
+    sigma=0.05 
+    coordinate=c(1,1, -1,-1, -1,1, 1,-1)
+    
+    cl = c(rep(1,35), rep(6, 5), 
+           rep(2,35), rep(7, 5), 
+           rep(3,35), rep(8, 5), 
+           rep(4,35), rep(9, 5), 
+           rep(5, 20))
+    
+    mat = matrix(runif(sum(n)*2,-2,2), sum(n), 2)
+    colnames(mat) = c('y','x')
+    loca1 = 1:40;  loca2 = 41:80;  loca3 = 81:120; loca4=121:160; loca5 = 161:180
+    mat[loca1,1] = beta[1] * mat[loca1,2] + rnorm(40,0,sigma)
+    mat[loca2,1] = beta[2] * mat[loca2,2] + rnorm(40,0,sigma)
+    mat[loca3,1] = beta[3] * mat[loca3,2] + rnorm(40,0,sigma)
+    mat[loca4,1] = beta[4] * mat[loca4,2] + rnorm(40,0,sigma)
+    mat[loca5,1] = simu_outlier_k(mat[loca5, 2], beta, distOut=2)
+    mat = mat[,2:1] #change order
+    
+    # spatial coordinate
+    ccc = mvrnorm(n[1], mu = c(coordinate[1],coordinate[2]), Sigma=diag(0.1, 2,2))
+    ccc2 = mvrnorm(n[2], mu = c(coordinate[3],coordinate[4]), Sigma=diag(0.1, 2,2))
+    ccc3 = mvrnorm(n[3], mu = c(coordinate[5],coordinate[6]), Sigma=diag(0.1, 2,2))
+    ccc4 = mvrnorm(n[4], mu = c(coordinate[7],coordinate[8]), Sigma=diag(0.1, 2,2))
+    ccc.o1 = mvrnorm(5, mu = c(coordinate[1],coordinate[2]), Sigma=diag(0.1, 2,2))
+    ccc.o2 = mvrnorm(5, mu = c(coordinate[3],coordinate[4]), Sigma=diag(0.1, 2,2))
+    ccc.o3 = mvrnorm(5, mu = c(coordinate[5],coordinate[6]), Sigma=diag(0.1, 2,2))
+    ccc.o4 = mvrnorm(5, mu = c(coordinate[7],coordinate[8]), Sigma=diag(0.1, 2,2))
+    ccc.n1 = rbind(ccc[1:35,], ccc2[36:40,])
+    ccc.n2 = rbind(ccc2[1:35,], ccc3[36:40,])
+    ccc.n3 = rbind(ccc3[1:35,], ccc4[36:40,])
+    ccc.n4 = rbind(ccc4[1:35,], ccc[36:40,])
+    xy = rbind(ccc.n1,ccc.n2,ccc.n3,ccc.n4,  ccc.o1,ccc.o2,ccc.o3,ccc.o4)
+  }
+  
   
   return(list(mat=mat, xy=xy, cl=cl))
 }
